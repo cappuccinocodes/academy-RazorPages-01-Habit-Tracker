0d7aed5931bbda18b168051d0efe6db94f6a696a
pablo-the-souza <53923086+pablo-the-souza@users.noreply.github.com>
2022-04-13 07:01:02 +1000

    Javascript demo

--- HabitTracker/wwwroot/js/site.js
+++ HabitTracker/wwwroot/js/site.js
@@ -1,5 +1,11 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function calculate() {
+    var tbl = document.getElementById("records");
+    var result = 0;
 
-// Write your JavaScript code.
-
+    for (var i = 1; i < tbl.rows.length; i++) {
+        //result = result + row.cells[1].innerHTML;
+        result = result + +tbl.rows[i].cells[1].innerHTML;
+    }
+    var resultArea = document.getElementById("result");
+    resultArea.append(`${result}`)
+}
